a2e62cfa6048b103104118dc0727ff15f3f4ef06
sxjscience <xshiab@ust.hk>
2015-11-12 12:57:04 +0800

    Revised DQN

--- example/dqn/dqn.py
+++ example/dqn/dqn.py
@@ -4,9 +4,6 @@ import mxnet as mx
 import numpy
 from defaults import *
 
-def dqn_loss(qval):
-    return numpy.argmax(qval, axis=1)
-
 def dqn_out_grad(qval, action, reward):
     if isinstance(qval, mx.ndarray.NDArray):
         qval = qval.asnumpy()
@@ -15,7 +12,9 @@ def dqn_out_grad(qval, action, reward):
     out_grad[numpy.arange(qval.shape[0]), action] = numpy.clip(qval[numpy.arange(action.shape[0]), action] - reward, -1, 1)
     return out_grad
 
-# def dqn_action(qval, exploration_prob=Defaults.EXPLORATION_EPSILON_DECAY):
+def dqn_action(qval, exploration_prob=Defaults.EXPLORATION_EPSILON_DECAY):
+    rng = get_numpy_rng()
+    exploration_prob = rng.choice([0, 1], size=qval.shape[0], p=[.1, .9])
 
 
 def dqn_network(action_num=4):
@@ -24,6 +23,7 @@ def dqn_network(action_num=4):
     net = mx.symbol.Activation(data=net, name='relu1', act_type="relu")
     net = mx.symbol.Convolution(data=net, name='conv2', kernel=(4, 4), stride=(2, 2), num_filter=32)
     net = mx.symbol.Activation(data=net, name='relu2', act_type="relu")
+    net = mx.symbol.Flatten(data=net)
     net = mx.symbol.FullyConnected(data=net, name='fc3', num_hidden=256)
     net = mx.symbol.Activation(data=net, name='relu3', act_type="relu")
     net = mx.symbol.FullyConnected(data=net, name='fc4', num_hidden=action_num)
