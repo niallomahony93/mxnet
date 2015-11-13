d929640d6b3ec8a9c572fa919de95cea59bd40a6
sxjscience <xshiab@ust.hk>
2015-11-13 23:50:56 +0800

    Update DQN

--- example/dqn/dqn.py
+++ example/dqn/dqn.py
@@ -1,26 +1,35 @@
 __author__ = 'sxjscience'
 
 import mxnet as mx
+from mxnet import metric
 import numpy
 from defaults import *
+import logging
 
 class DQNOutputOp(mx.operator.NumpyOp):
     def __init__(self):
         super(DQNOutputOp, self).__init__(need_top_grad=False)
+
     def list_arguments(self):
         return ['data', 'action', 'reward']
+
     def list_outputs(self):
         return ['output']
+
     def infer_shape(self, in_shape):
         data_shape = in_shape[0]
         action_shape = in_shape[1]
         reward_shape = (in_shape[0][0],)
         output_shape = (in_shape[0][0],)
         return [data_shape, action_shape, reward_shape], [output_shape]
+
     def forward(self, in_data, out_data):
         x = in_data[0]
         y = out_data[0]
-        y[:] = numpy.argmax(x, axis=1)
+        action = in_data[1]
+        action = action.flatten().astype(numpy.int)
+        y[:] = x[numpy.arange(action.shape[0]), action].flatten()
+
     def backward(self, out_grad, in_data, out_data, in_grad):
         x = in_data[0]
         action = in_data[1]
@@ -29,6 +38,17 @@ class DQNOutputOp(mx.operator.NumpyOp):
         dx = in_grad[0]
         dx[:] = 0
         dx[numpy.arange(action.shape[0]), action] = numpy.clip(x[numpy.arange(action.shape[0]), action] - reward, -1, 1)
+
+
+class DQNInitializer(mx.initializer.Xavier):
+    def _init_bias(self, _, arr):
+        arr[:] = .1
+
+def dqn_callback(param):
+    return
+
+def dqn_metric(label, pred):
+    return abs(label.asnumpy() - pred.asnumpy()).sum()
 
 def dqn_network(action_num=4):
     net = mx.symbol.Variable('data')
@@ -45,9 +65,22 @@ def dqn_network(action_num=4):
     net = DQNOutput(data=net, action=action, name='dqn')
     return net
 
-data_shape = (10, 4, 84, 84)
+data_shape = (5, 4, 84, 84)
+action_shape = (5,)
 dqn_sym = dqn_network()
-print dqn_sym.list_arguments()
-dqn_executor = dqn_sym.simple_bind(ctx=get_ctx(), data=data_shape)
+X = mx.random.uniform(0, 10, data_shape)
+Y = mx.random.uniform(0, 1, action_shape)*3
+R = mx.random.uniform(0, 10, action_shape)
+d = {'data':data_shape, 'action':action_shape, 'dqn_reward': action_shape}
+logging.basicConfig(level=logging.DEBUG)
 
+model = mx.model.FeedForward(symbol=dqn_sym, ctx=get_ctx(),initializer=DQNInitializer(),
+                             num_epoch=100, numpy_batch_size=5,
+                             learning_rate=0.0001, momentum=0.9, wd = 0.00001)
+print model.arg_params
+iter = mx.io.NDArrayIter({'data':X, 'action':Y}, {'dqn_reward':R}, batch_size=5)
+model.fit(X=iter, eval_metric=mx.metric.CustomMetric(dqn_metric), batch_end_callback=dqn_callback)
+iter = mx.io.NDArrayIter({'data':X, 'action':None}, batch_size=5)
+print model.predict(iter)
+print R.asnumpy()
 
