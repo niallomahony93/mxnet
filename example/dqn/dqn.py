442480607edfefb8da01ed0a04f8f87f2706cf36
sxjscience <xshiab@ust.hk>
2015-11-14 21:51:58 +0800

    Update DQN

--- example/dqn/dqn.py
+++ example/dqn/dqn.py
@@ -6,6 +6,7 @@ import numpy
 from defaults import *
 import logging
 
+
 class DQNOutputOp(mx.operator.NumpyOp):
     def __init__(self):
         super(DQNOutputOp, self).__init__(need_top_grad=False)
@@ -41,8 +42,6 @@ class DQNInitializer(mx.initializer.Xavier):
     def _init_bias(self, _, arr):
         arr[:] = .1
 
-def dqn_callback(param):
-    return
 
 def dqn_metric(action_reward, qvec):
     action_reward_npy = action_reward.asnumpy()
@@ -50,6 +49,7 @@ def dqn_metric(action_reward, qvec):
     action_npy = action_reward_npy[:, 0].astype(numpy.int)
     reward_npy = action_reward_npy[:, 1]
     return abs(qvec_npy[numpy.arange(action_npy.shape[0]), action_npy] - reward_npy).sum()
+
 
 def dqn_network(action_num=4):
     net = mx.symbol.Variable('data')
@@ -65,31 +65,53 @@ def dqn_network(action_num=4):
     net = DQNOutput(data=net, name='dqn')
     return net
 
+
+class DQN(object):
+    def __init__(self):
+        data_shape = (5, 4, 84, 84)
+        action_reward_shape = (5, 2)
+        d = {'data': data_shape, 'dqn_action_reward': action_reward_shape}
+        self.dqn_sym = dqn_network()
+        self.shortcut_dqn_sym = dqn_network()
+        self.online_net = mx.model.FeedForward(symbol=self.dqn_sym, ctx=get_ctx(), initializer=DQNInitializer(),
+                                               num_epoch=100, numpy_batch_size=5,
+                                               learning_rate=0.0001, momentum=0.9, wd=0.00001)
+        self.online_net._init_params(d)
+        self.shortcut_net = mx.model.FeedForward(symbol=self.dqn_sym, ctx=get_ctx(), initializer=DQNInitializer(),
+                                                 numpy_batch_size=5, arg_params=self.online_net.arg_params)
+        self.shortcut_net._init_predictor({'data': data_shape})
+        self.update_shortcut()
+
+    def update_shortcut(self):
+        self.shortcut_net._pred_exec.copy_params_from(self.online_net.arg_params, self.online_net.aux_params)
+
+    def fit(self, iter):
+        self.online_net.fit(X=iter, eval_metric=mx.metric.CustomMetric(dqn_metric),
+                            batch_end_callback=self.dqn_batch_callback)
+
+    def dqn_batch_callback(self, param):
+        print param
+        return
+
+    def dqn_epoch_end_callback(self, epoch, symbol, arg_params, aux_states):
+        return
+
+
+logging.basicConfig(level=logging.DEBUG)
 data_shape = (5, 4, 84, 84)
 action_shape = (5,)
 action_reward_shape = (5, 2)
-dqn_sym = dqn_network()
 X = mx.random.uniform(0, 10, data_shape)
 Y = mx.random.uniform(0, 3, action_shape)
 R = mx.random.uniform(0, 10, action_shape)
 Y_R = mx.ndarray.empty(action_reward_shape)
 Y_R[:] = numpy.vstack((Y.asnumpy(), R.asnumpy())).T
-d = {'data':data_shape, 'dqn_action_reward': action_reward_shape}
-logging.basicConfig(level=logging.DEBUG)
-
-model = mx.model.FeedForward(symbol=dqn_sym, ctx=get_ctx(),initializer=DQNInitializer(),
-                             num_epoch=100, numpy_batch_size=5,
-                             learning_rate=0.0001, momentum=0.9, wd = 0.00001)
-arg_shapes, _, aux_shapes = model.symbol.infer_shape(**d)
-arg_names, param_names, aux_names = model._init_params(d)
-iter = mx.io.NDArrayIter({'data':X}, {'dqn_action_reward':Y_R}, batch_size=5)
-print arg_names
-print param_names
-print aux_names
-
-model.fit(X=iter, eval_metric=mx.metric.CustomMetric(dqn_metric), batch_end_callback=dqn_callback)
-Y_new = mx.ndarray.zeros(action_shape)
-iter = mx.io.NDArrayIter({'data':X}, batch_size=5)
-print model.predict(iter)
+iter = mx.io.NDArrayIter({'data': X}, {'dqn_action_reward': Y_R}, batch_size=5)
+dqn = DQN()
+W = mx.random.uniform(0, 10, (10, 4, 84, 84))
+print dqn.shortcut_net.predict(W)
+dqn.fit(iter)
+print dqn.online_net.predict(W)
+dqn.update_shortcut()
+print dqn.shortcut_net.predict(W)
 print R.asnumpy()
-
