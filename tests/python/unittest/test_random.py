8bc9d9dd548cb2e19a51fb33336695ca3925ba64
sxjscience <xshiab@ust.hk>
2016-01-15 20:54:14 +0800

    Revert "Update random number generator test"
    This reverts commit b63bda37aa2e9b5251cf6c54d59785d2856659ca.

--- tests/python/unittest/test_random.py
+++ tests/python/unittest/test_random.py
@@ -9,19 +9,18 @@ def check_with_device(device):
     with mx.Context(device):
         a, b = -10, 10
         mu, sigma = 10, 2
-        for i in range(5):
-            shape = (100 + i, 100 + i)
-            mx.random.seed(128)
-            ret1 = mx.random.normal(mu, sigma, shape)
-            un1 = mx.random.uniform(a, b, shape)
-            mx.random.seed(128)
-            ret2 = mx.random.normal(mu, sigma, shape)
-            un2 = mx.random.uniform(a, b, shape)
-            assert same(ret1.asnumpy(), ret2.asnumpy())
-            assert same(un1.asnumpy(), un2.asnumpy())
-            assert abs(np.mean(ret1.asnumpy()) - mu) < 0.1
-            assert abs(np.std(ret1.asnumpy()) - sigma) < 0.1
-            assert abs(np.mean(un1.asnumpy()) - (a+b)/2) < 0.1
+        shape = (100, 100)
+        mx.random.seed(128)
+        ret1 = mx.random.normal(mu, sigma, shape)
+        un1 = mx.random.uniform(a, b, shape)
+        mx.random.seed(128)
+        ret2 = mx.random.normal(mu, sigma, shape)
+        un2 = mx.random.uniform(a, b, shape)
+        assert same(ret1.asnumpy(), ret2.asnumpy())
+        assert same(un1.asnumpy(), un2.asnumpy())
+        assert abs(np.mean(ret1.asnumpy()) - mu) < 0.1
+        assert abs(np.std(ret1.asnumpy()) - sigma) < 0.1
+        assert abs(np.mean(un1.asnumpy()) - (a+b)/2) < 0.1
 
 
 def test_random():
