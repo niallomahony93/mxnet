7c80ead58fa53bd8b86914ba96fc3e0269d403c5
sxjscience <xshiab@ust.hk>
2016-03-01 17:05:14 +0800

    Update testing script

--- example/dqn/tests/test_atari_game.py
+++ example/dqn/tests/test_atari_game.py
@@ -1,6 +1,8 @@
 from ..games import AtariGame
 import numpy
 import time
+import mxnet as mx
+import mxnet.ndarray as nd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
@@ -8,11 +10,12 @@ replay_start_size = 1000
 game = AtariGame(resize_mode='scale', replay_start_size=replay_start_size)
 game.start()
 start = time.time()
-totoal_time_step = 5000
+totoal_time_step = 10000
 minibatch_size = 32
 sample_total_time = 0
 sample_total_num = 0
 plt.figure()
+
 
 for i in xrange(totoal_time_step):
     action = numpy.random.randint(0, len(game.action_set))
@@ -23,6 +26,10 @@ for i in xrange(totoal_time_step):
     if game.replay_memory.sample_enabled:
         sample_start = time.time()
         states, actions, rewards, next_states, terminate_flags = game.replay_memory.sample(minibatch_size)
+        gpu_states = nd.array(states, ctx=mx.gpu())/255
+        gpu_next_states = nd.array(next_states, ctx=mx.gpu())/255
+        #plt.imshow(gpu_states.asnumpy()[0,0,:,:] * 255, cmap=cm.Greys_r)
+        #plt.show()
         sample_end = time.time()
         sample_total_time += sample_end - sample_start
         sample_total_num += 1
