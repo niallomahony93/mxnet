a26d6e3b4f5a3b6bfba551179ae5abf787b3316d
sxjscience <xshiab@ust.hk>
2016-03-02 12:46:35 +0800

    We need now to start a new episodic run of the game by calling game.begin_episode(max_step)

--- example/dqn/games/atari_games.py
+++ example/dqn/games/atari_games.py
@@ -15,6 +15,7 @@ _numpy_rng = numpy.random.RandomState(123456)
 _dirname = os.path.dirname(os.path.realpath(__file__))
 _default_rom_path = os.path.join(_dirname, "roms", "breakout.bin")
 
+
 def ale_load_from_rom(rom_path, display_screen):
     rng = _numpy_rng
     ale = ALEInterface()
@@ -25,17 +26,24 @@ def ale_load_from_rom(rom_path, display_screen):
     return ale
 
 
+DEFAULT_MAX_EPISODE_STEP = 1000000
+
+
 class AtariGame(object):
     def __init__(self,
                  rom_path=_default_rom_path,
                  frame_skip=4, history_length=4,
                  resize_mode='scale', resized_rows=84, resized_cols=84, crop_offset=8,
                  display_screen=False, max_null_op=30,
-                 replay_memory_size=1000000, replay_start_size=100):
+                 replay_memory_size=1000000,
+                 replay_start_size=100,
+                 death_end_episode=True):
         self.total_reward = 0
-        self.current_step = 0
+        self.current_episode_step = 0
+        self.max_episode_step = DEFAULT_MAX_EPISODE_STEP
         self.rng = _numpy_rng
         self.ale = ale_load_from_rom(rom_path=rom_path, display_screen=display_screen)
+        self.start_lives = self.ale.lives()
         self.action_set = self.ale.getMinimalActionSet()
         self.resize_mode = resize_mode
         self.resized_rows = resized_rows
@@ -44,6 +52,7 @@ class AtariGame(object):
         self.frame_skip = frame_skip
         self.history_length = history_length
         self.max_null_op = max_null_op
+        self.death_end_episode = death_end_episode
         self.screen_buffer_length = 2
         self.screen_buffer = numpy.empty((self.screen_buffer_length,
                                           self.ale.getScreenDims()[1], self.ale.getScreenDims()[0]),
@@ -62,7 +71,29 @@ class AtariGame(object):
             self.ale.act(0)
             self.ale.getScreenGrayscale(self.screen_buffer[i % self.screen_buffer_length, :, :])
         self.total_reward = 0
-        self.current_step = 0
+        self.current_episode_step = 0
+        self.max_episode_step = DEFAULT_MAX_EPISODE_STEP
+        self.start_lives = self.ale.lives()
+
+    '''
+    Call this to begin an episode of a game instance.
+    Here, we can play the game for a maximum of `max_episode_step` and after that, we are force to
+    '''
+
+    def begin_episode(self, max_episode_step=DEFAULT_MAX_EPISODE_STEP):
+        # We need to restart the environment if our current counting is larger than the max episode_step
+        if self.current_episode_step > self.max_episode_step or self.ale.game_over():
+            self.start()
+        self.max_episode_step = max_episode_step
+        self.start_lives = self.ale.lives()
+
+    @property
+    def episode_terminate(self):
+        termination_flag = self.ale.game_over() or self.current_episode_step > self.max_episode_step
+        if self.death_end_episode:
+            return (self.ale.lives() < self.start_lives) or termination_flag
+        else:
+            return termination_flag
 
     def get_observation(self):
         image = self.screen_buffer.max(axis=0)
@@ -80,11 +111,18 @@ class AtariGame(object):
             return cv2.resize(image, (self.resized_cols, self.resized_rows),
                               interpolation=cv2.INTER_LINEAR)
 
+    @property
+    def state_enabled(self):
+        return self.replay_memory.size >= self.replay_memory.history_length
+
     def current_state(self):
         return self.replay_memory.latest_slice()
 
     def play(self, a):
-        self.current_step += 1
+        assert not self.episode_terminate, "Warning, the episode seems to have terminated. " \
+                                                "We need to call either game.begin_episode(max_episode_step) to continue a new episode" \
+                                                "or game.start() to force restart."
+        self.current_episode_step += 1
         reward = 0.0
         action = self.action_set[a]
         for i in xrange(self.frame_skip):
@@ -92,6 +130,6 @@ class AtariGame(object):
             self.ale.getScreenGrayscale(self.screen_buffer[i % self.screen_buffer_length, :, :])
         self.total_reward += reward
         ob = self.get_observation()
-        terminate_flag = self.ale.game_over()
+        terminate_flag = self.episode_terminate
         self.replay_memory.append(ob, a, numpy.clip(reward, -1, 1), terminate_flag)
         return reward, terminate_flag
--- example/dqn/tests/test_atari_game.py
+++ example/dqn/tests/test_atari_game.py
@@ -8,9 +8,9 @@ import matplotlib.cm as cm
 
 replay_start_size = 1000
 game = AtariGame(resize_mode='scale', replay_start_size=replay_start_size)
-game.start()
+game.begin_episode()
 start = time.time()
-totoal_time_step = 1000000
+totoal_time_step = 10000
 minibatch_size = 32
 sample_total_time = 0
 sample_total_num = 0
@@ -21,13 +21,13 @@ for i in xrange(totoal_time_step):
     action = numpy.random.randint(0, len(game.action_set))
     reward, terminate_flag = game.play(action)
     if terminate_flag:
-        game.start()
+        game.begin_episode()
     current_state = game.current_state()
     if game.replay_memory.sample_enabled:
         sample_start = time.time()
         states, actions, rewards, next_states, terminate_flags = game.replay_memory.sample(minibatch_size)
-        gpu_states = nd.array(states, ctx=mx.gpu())/255
-        gpu_next_states = nd.array(next_states, ctx=mx.gpu())/255
+        gpu_states = nd.array(states, ctx=mx.gpu())/float(255.0)
+        gpu_next_states = nd.array(next_states, ctx=mx.gpu())/float(255.0)
         #plt.imshow(gpu_states.asnumpy()[0,0,:,:] * 255, cmap=cm.Greys_r)
         #plt.show()
         sample_end = time.time()
