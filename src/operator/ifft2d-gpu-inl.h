e1a96204f3db95e823a769b23d8518be0fd8c4b3
sxjscience <xshiab@ust.hk>
2016-04-29 23:42:03 +0800

    Remove backward dependencies

--- src/operator/ifft2d-gpu-inl.h
+++ src/operator/ifft2d-gpu-inl.h
@@ -155,7 +155,7 @@ namespace mxnet {
         const std::vector<int> &out_grad,
         const std::vector<int> &in_data,
         const std::vector<int> &out_data) const override {
-        return{ out_data[ifft2d::kOut] };
+        return{ };
       }
 
       Operator* CreateOperator(Context ctx) const override;
