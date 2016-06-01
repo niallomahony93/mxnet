Update test script + Fix implemenation of Reduce/Broadcast

tests/python/unittest/test_ndarray.py
@@ -208,8 +208,8 @@ def test_reduce():
     sample_num = 1000
     def test_reduce_inner(numpy_reduce_func, nd_reduce_func):
         for i in range(sample_num):
-            ndim = np.random.randint(1, 6)
-            shape = np.random.randint(1, 16, size=ndim)
+            ndim = np.random.randint(1, 8)
+            shape = np.random.randint(1, 11, size=ndim)
             axis_flags = np.random.randint(0, 2, size=ndim)
             axes = []
             for (axis, flag) in enumerate(axis_flags):
@@ -240,8 +240,8 @@ def test_broadcast():
     sample_num = 1000
     def test_broadcast_to():
         for i in range(sample_num):
-            ndim = np.random.randint(1, 6)
-            target_shape = np.random.randint(1, 16, size=ndim)
+            ndim = np.random.randint(1, 8)
+            target_shape = np.random.randint(1, 11, size=ndim)
             shape = target_shape.copy()
             axis_flags = np.random.randint(0, 2, size=ndim)
             axes = []

tests/python/unittest/test_operator.py
@@ -921,100 +921,13 @@ def test_reshape():
     _, output_shape, __ = net.infer_shape(data=(2, 3, 5, 5))
     assert(output_shape[0] == (2, 75))
 
-
-def test_sum_mid_internal():
-    a = mx.symbol.Variable('a')
-    b = mx.symbol.sum_mid_internal(a)
-
-    for i in range(1, 10):
-        for j in range(1, 10):
-            for k in range(1, 10):
-                a_npy = np.random.rand(i, j, k)
-                a_grad = mx.nd.empty((i, j, k))
-                b_grad_npy = np.random.rand(i, k)
-                net = b.bind(mx.cpu(), args={'a': mx.nd.array(a_npy)},
-                             args_grad={'a': a_grad})
-                net.forward(is_train=True)
-                assert np.square(net.outputs[0].asnumpy() - a_npy.sum(axis=1)).mean() < 1E-6,\
-                    np.square(net.outputs[0].asnumpy() - a_npy.sum(axis=1)).mean()
-                net.backward(out_grads=mx.nd.array(b_grad_npy))
-                assert np.square(a_grad.asnumpy() - b_grad_npy.reshape((i, 1, k))).mean() < 1E-6
-
-def test_reduce():
-    sample_num = 1000
-    def test_reduce_inner(numpy_reduce_func, mx_reduce_sym):
-        for i in range(sample_num):
-            # Generate random data that has ndim between 1-5 and all the shape dims between 1-14
-            ndim = np.random.randint(1, 6)
-            shape = np.random.randint(1, 15, size=(ndim,))
-            axis_num = np.random.randint(0, ndim, size=1)
-            axis_flags = np.random.randint(0, 2, size=ndim)
-            axes = []
-            for (axis, flag) in enumerate(axis_flags):
-                if flag:
-                    axes.append(axis)
-            if 0 == len(axes):
-                axes = tuple(range(ndim))
-            else:
-                axes = tuple(axes)
-            keepdims = np.random.randint(0, 2)
-            a = mx.symbol.Variable('a')
-            b = mx_reduce_sym(a, axis=axes, keepdims=keepdims)
-            dat_npy = np.random.rand(*shape)
-            sum_groundtruth = np.array(numpy_reduce_func(dat_npy, axis=axes, keepdims=keepdims))
-            if sum_groundtruth.shape == ():
-                sum_groundtruth = np.array([sum_groundtruth])
-            grad_nd = mx.nd.empty(shape)
-            outgrad_npy = np.array(np.random.rand(*sum_groundtruth.shape))
-            grad_groundtruth = np.broadcast_to(outgrad_npy.reshape(dat_npy.sum(axis=axes, keepdims=1).shape),
-                                               dat_npy.shape)
-            net = b.bind(mx.cpu(), args={'a': mx.nd.array(dat_npy)},
-                         args_grad={'a': grad_nd})
-            net.forward(is_train=True)
-
-            err_forward = np.square(net.outputs[0].asnumpy() - sum_groundtruth).mean()
-            assert err_forward < 1E-4
-            net.backward(out_grads=mx.nd.array(outgrad_npy))
-            err_backward = np.square(grad_nd.asnumpy() - grad_groundtruth).sum()
-            assert err_backward < 1E-6
-    test_reduce_inner(np.sum, mx.symbol.sum)
-
-def test_broadcast():
-    sample_num = 1000
-    def test_broadcast_axis():
-        for i in range(sample_num):
-            # Generate random data that has ndim between 1-5 and all the shape dims between 1-14
-            ndim = np.random.randint(1, 6)
-            target_shape = np.random.randint(1, 15, size=(ndim,))
-            axis = np.random.randint(0, ndim)
-            shape = target_shape.copy()
-            size = shape[axis]
-            shape[axis] = 1
-            a = mx.symbol.Variable('a')
-            b = mx.symbol.broadcast_axis(a, axis=axis, size=size)
-            dat_npy = np.random.rand(*shape)
-            groundtruth = np.broadcast_to(dat_npy, shape=target_shape)
-            grad_nd = mx.nd.empty(shape)
-            outgrad_npy = np.random.rand(*target_shape)
-            grad_groundtruth = np.sum(outgrad_npy, axis=axis, keepdims=True)
-            net = b.bind(mx.cpu(), args={'a': mx.nd.array(dat_npy)},
-                         args_grad={'a': grad_nd})
-            net.forward(is_train=True)
-            assert (net.outputs[0].shape == target_shape).all()
-            err_forward = np.square(net.outputs[0].asnumpy() - groundtruth).mean()
-            assert err_forward < 1E-8
-            net.backward(out_grads=mx.nd.array(outgrad_npy))
-            err_backward = np.square(grad_nd.asnumpy() - grad_groundtruth).mean()
-            assert err_backward < 1E-8
-    test_broadcast_axis()
-
 def test_reduce():
     sample_num = 1000
     def test_reduce_inner(numpy_reduce_func, numpy_reduce_grad_func, mx_reduce_sym):
         for i in range(sample_num):
-            # Generate random data that has ndim between 1-5 and all the shape dims between 1-14
-            ndim = np.random.randint(1, 6)
-            shape = np.random.randint(1, 15, size=(ndim,))
+            # Generate random data that has ndim between 1-7 and all the shape dims between 1-10
+            ndim = np.random.randint(1, 8)
+            shape = np.random.randint(1, 11, size=(ndim,))
             axis_num = np.random.randint(0, ndim, size=1)
             axis_flags = np.random.randint(0, 2, size=ndim)
             axes = []
@@ -1054,9 +967,9 @@ def test_broadcast():
     sample_num = 1000
     def test_broadcast_axis():
         for i in range(sample_num):
-            # Generate random data that has ndim between 1-5 and all the shape dims between 1-14
-            ndim = np.random.randint(1, 6)
-            target_shape = np.random.randint(1, 15, size=(ndim,))
+            # Generate random data that has ndim between 1-7 and all the shape dims between 1-10
+            ndim = np.random.randint(1, 8)
+            target_shape = np.random.randint(1, 11, size=(ndim,))
             axis = np.random.randint(0, ndim)
             shape = target_shape.copy()
             size = shape[axis]
@@ -1106,7 +1019,6 @@ if __name__ == '__main__':
     check_softmax_with_ignore_label(mx.cpu())
     test_convolution_dilated_impulse_response()
     test_reshape()
-    test_sum_mid_internal()
     test_reduce()
     test_broadcast()
     #check_softmax_with_shape((3,4), mx.cpu())