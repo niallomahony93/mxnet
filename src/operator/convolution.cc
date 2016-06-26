d0389c93ca4339fa90bc2b8f66a24d25a1f45eb7
sxjscience <xshiab@ust.hk>
2016-06-26 16:36:11 +0800

    Add more descriptions

--- src/operator/convolution.cc
+++ src/operator/convolution.cc
@@ -31,9 +31,11 @@ Operator *ConvolutionProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in
 DMLC_REGISTER_PARAMETER(ConvolutionParam);
 
 MXNET_REGISTER_OP_PROPERTY(Convolution, ConvolutionProp)
-.add_argument("data", "Symbol", "Input data to the ConvolutionOp.")
-.add_argument("weight", "Symbol", "Weight matrix.")
-.add_argument("bias", "Symbol", "Bias parameter.")
+.add_argument("data", "Symbol", "Input data to the ConvolutionOp."
+              " Shape: (batchsize, input channels, height, width).")
+.add_argument("weight", "Symbol", "Weight matrix."
+              " Shape: (num filters, input channels, filter height, filter width).")
+.add_argument("bias", "Symbol", "Bias parameter. Shape: (num filters,).")
 .add_arguments(ConvolutionParam::__FIELDS__())
 .describe("Apply convolution to input then add a bias.");
 
