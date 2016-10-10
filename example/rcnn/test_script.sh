4dcb84e403f6663dfc0ef270051a9f296f260fc9
sxjscience <xshiab@ust.hk>
2016-10-10 21:13:11 +0800

    revise running/testing command

---
+++ example/rcnn/test_script.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python test.py --has_rpn --prefix ./final --epoch 0 --devkit_path E:\\datasets\\VOCdevkit --gpu 0
---
+++ example/rcnn/train_script.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python -u train_end2end.py --devkit_path E:\\datasets\\VOCdevkit --load-epoch 1 --gpu 0
