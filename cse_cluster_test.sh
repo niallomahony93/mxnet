7230cbc86a9ce5e4cd13f440803b6b76af8d2b74
sxjscience <xshiab@ust.hk>
2017-04-29 01:55:47 +0800

    add testing script

---
+++ cse_cluster_test.sh
@@ -0,0 +1,16 @@
+#!/bin/csh
+
+# specify where the output file should be put
+#$ -o /project/dygroup2/xingjian/mxnet
+
+# specify the working path
+#$ -wd /project/dygroup2/xingjian/mxnet
+
+# email me with this address...
+#$ -M xshiab@connect.ust.hk
+# email when the job starts (b) and after the job has been
+# completed (e)
+#$ -m be
+source ~/.cshrc_user
+cd tests/python/gpu
+nosetests test_operator_gpu.py
