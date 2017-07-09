20f5ff53e07a57c83de32e33109faf7633f2ce4c
sxjscience <xshiab@ust.hk>
2017-07-09 17:54:29 +0800

    update

--- cse_cluster_opencv.sh
+++ cse_cluster_opencv.sh
@@ -4,7 +4,7 @@
 #$ -o /project/dygroup2/xingjian/mxnet
 
 # specify the working path
-#$ -wd /project/dygroup2/xingjian/mxnet
+#$ -wd /project/dygroup2/xingjian/opencv-3.2.0/build
 
 #$ -l h=client111
 
@@ -14,7 +14,6 @@
 # completed (e)
 #$ -m be
 source ~/.cshrc_user
-cd /project/dygroup2/xingjian/opencv-3.2.0/build
-#rm -r *
-#cmake -D BUILD_opencv_gpu=OFF -D WITH_CUDA=OFF -D WITH_1394=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/project/dygroup2/xingjian/.local ..
+##rm -r *
+##cmake -D BUILD_opencv_gpu=OFF -D WITH_CUDA=OFF -D WITH_1394=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/project/dygroup2/xingjian/.local ..
 make -j64
