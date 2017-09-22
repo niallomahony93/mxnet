371e8f023b3b9c3fb30a70ea4a7f957f0dcd8dcc
sxjscience <xshiab@ust.hk>
2017-09-22 11:17:24 +0800

    fix bug

--- src/operator/tensor/nn_addition_op.cu
+++ src/operator/tensor/nn_addition_op.cu
@@ -26,7 +26,7 @@ NNVM_REGISTER_OP(BSN)
 .set_attr<FCompute>("FCompute<gpu>", BinaryStochasticNeuronCompute<gpu>);
 
 NNVM_REGISTER_OP(_backward_BSN)
-.set_attr<FCompute>("FCompute<gpu>", BinaryCompute<gpu, unary_bwd<mshadow_op::sigmoid_grad>>);
+.set_attr<FCompute>("FCompute<gpu>", ElemwiseBinaryOp::Compute<gpu, unary_bwd<mshadow_op::sigmoid_grad>>);
 
 NNVM_REGISTER_OP(argsort_last)
 .set_attr<FCompute>("FCompute<gpu>", ArgSortLast<gpu>);
