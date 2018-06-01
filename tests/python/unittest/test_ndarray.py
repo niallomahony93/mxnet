df93779440cb80c79850b2f522a0ead8b308db70
sxjscience <sxjscience001@gmail.com>
2018-06-01 22:10:34 +0800

    add test

--- tests/python/unittest/test_ndarray.py
+++ tests/python/unittest/test_ndarray.py
@@ -640,9 +640,9 @@ def test_order():
     # the mxnet operator.  The 'mask' function is particularly hard to test given that
     # equal values might span the 'k' boundary.  Issue exposed with seed 1405838964.
     def get_values(ensure_unique, dtype):
-        if dtype == np.int16 or dtype == np.int32:
+        if dtype == np.int16 or dtype == np.int32 or dtype == np.int64:
             return np.arange(dat_size ** 4, dtype=dtype).reshape((dat_size, dat_size, dat_size, dat_size))
-        elif dtype == np.float32:
+        elif dtype == np.float32 or dtype == np.float64:
             while True:
                 data = np.random.normal(size=(dat_size, dat_size, dat_size, dat_size)).astype(dtype)
                 if not ensure_unique:
@@ -650,8 +650,10 @@ def test_order():
                 num_unique_values = len(set(data.flatten()))
                 if data.size == num_unique_values:
                     return data
+        else:
+            raise NotImplementedError
 
-    for dtype in [np.int16, np.int32, np.float32, np.float64]:
+    for dtype in [np.int16, np.int32, np.int64, np.float32, np.float64]:
         a_npy = get_values(ensure_unique=True, dtype=dtype)
         a_nd = mx.nd.array(a_npy, ctx=ctx)
 
@@ -723,15 +725,30 @@ def test_order():
     k = 10
     with mx.autograd.record():
         b = mx.nd.topk(a, k=k, ret_typ='value')
-        b.backward(mx.nd.ones((k,)))
+        b.backward(mx.nd.ones((k,), dtype=np.int32))
     a_grad = a.grad.asnumpy()
     for i in range(-1, - k - 1, -1):
         assert a_grad[i] == 1
 
+    # test topk gradient with a small shape
+    for dtype in [np.int32, np.int64, np.float32, np.float64]:
+        a = mx.nd.arange(0, 1000, step=1, repeat=1, dtype=dtype)
+        a.attach_grad()
+        k = 10
+        ograd = mx.nd.arange(0, k, dtype=dtype)
+        with mx.autograd.record():
+            b = mx.nd.topk(a, k=k, ret_typ='value')
+            b.backward(ograd)
+        a_grad = a.grad.asnumpy()
+        ograd_npy = ograd.asnumpy()
+        for i in range(-1, - k - 1, -1):
+            assert a_grad[i] == ograd_npy[-i - 1]
+
+
     # Repeat those tests that don't involve indices.  These should pass even with
     # duplicated input data values (over many repeated runs with different random seeds,
     # this will be tested).
-    for dtype in [np.int16, np.int32, np.float32, np.float64]:
+    for dtype in [np.int16, np.int32, np.int64, np.float32, np.float64]:
         a_npy = get_values(ensure_unique=False, dtype=dtype)
         a_nd = mx.nd.array(a_npy, ctx=ctx)
 
